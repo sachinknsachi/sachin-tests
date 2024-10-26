1dd22df05cb5875093126c152b8e9066b9722145
SachinKn <106311617+sachinknsachi@users.noreply.github.com>
2024-10-26 11:23:49 +0530

    Added Data provider

--- src/test/java/org/example/CruiseOnlyFlow.java
+++ src/test/java/org/example/CruiseOnlyFlow.java
@@ -13,61 +13,15 @@ import org.testng.annotations.*;
 import java.io.File;
 
 public class CruiseOnlyFlow extends BaseTest {
-    @Test
-    public void test_1() {
-        System.out.println("This is test_1()");
-
-        WebDriver driver = new ChromeDriver();
-        WebdriverActions actions = new WebdriverActions(driver);
-
+    @Test(testName = "ODY-1 : Air Only Flow", dataProvider = "genericDataProvider")
+    public void test_1(String name, WebdriverActions actions) {
         actions.openUrl("https://uat.odysol.com/swift/cruise?siid=130386");
     }
 
-    @Test
-    public void test_2() {
-        System.out.println("This is test_2()");
-
-        WebDriver driver = new ChromeDriver();
-        WebdriverActions actions = new WebdriverActions(driver);
-
+    @Test(testName = "ODY-2 : Cruise Only Flow", dataProvider = "genericDataProvider")
+    public void cruiseOnlyFlow(String name, WebdriverActions actions) {
+        System.out.println(name);
         actions.openUrl("https://uat.odysol.com/swift/cruise?siid=130385");
-    }
-
-    public static void main(String[] args) {
-
-        File file = new File("D:\\Work\\Git\\SachinKn-tutorials\\sachin-tests\\test-output\\AutomationReport.html");
-
-        ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(file);
-        extentSparkReporter.config().setReportName("Odysseus Report");
-        extentSparkReporter.config().setDocumentTitle("Odysseus Test Report");
-        extentSparkReporter.config().setTheme(Theme.DARK);
-
-        ExtentReports extentReports = new ExtentReports();
-        extentReports.attachReporter(extentSparkReporter);
-
-        ExtentTest extentTest1 = extentReports.createTest("ODY-1");
-        extentTest1.fail("Ody-1 passed");
-        extentTest1.pass("Ody-1 fail");
-        extentTest1.warning("Ody-1 warning");
-        extentTest1.skip("Ody-1 skip");
-        extentTest1.info("Ody-1 info");
-
-        ExtentTest extentTest2 = extentReports.createTest("ODY-2");
-        extentTest2.fail(MarkupHelper.createLabel("Ody-2 passed", ExtentColor.RED));
-        extentTest2.pass(MarkupHelper.createLabel("Ody-2 fail", ExtentColor.GREEN));
-        extentTest2.warning(MarkupHelper.createLabel("Ody-2 warning", ExtentColor.ORANGE));
-        extentTest2.skip(MarkupHelper.createLabel("Ody-2 skip", ExtentColor.YELLOW));
-        extentTest2.info(MarkupHelper.createLabel("Ody-2 info", ExtentColor.BROWN));
-
-        String[][] data = {
-                { "Name", "Age", "Place"},
-                { "Sachin", "18", "Banglore"},
-                { "Sanganna", "20", "Bagalkote"}
-        };
-        extentTest2.pass(MarkupHelper.createTable(data));
-
-        extentReports.flush();
-
     }
 
 }
